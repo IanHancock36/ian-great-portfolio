11096ed9010df2f502b4412bc6dedc4064f021ee
Ian Hancock <northroadian@gmail.com>
2021-06-10 07:16:31 -0500

    counter button works using redux in the app will remove this later this was practice

--- src/App.js
+++ src/App.js
@@ -8,10 +8,14 @@ import Projects from './Components/Projects'
 import Resume from './Components/Resume'
 import Testimonials from './Components/Testimonials'
 import './App.css'
-import {useSelector} from 'react-redux'
+import {useSelector ,useDispatch} from 'react-redux'
+import {increment, decrement} from './actions'
+
 
 function App() {
   const counter = useSelector( state => state.counter)
+  const isLogged = useSelector (state => state.isLogged )
+  const dispatch = useDispatch()
   return (
     <div className="App">
 
@@ -34,7 +38,12 @@ function App() {
        </Switch>
      </BrowserRouter>
      <h1>Counter {counter} </h1>
+     <button onClick={() => dispatch(increment())}> + </button>
+     <button onClick={()=> dispatch(decrement())}> - </button>
+    {isLogged? <h3>valuable info {isLogged}</h3> :'not logged in' }
+    
     </div>
+    // add button for is logged to toggle logged and unlogged 
   );
 }
 
