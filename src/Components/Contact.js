128dd24c5a23d34b6a56cd0b659620f5a552e560
Ian Hancock <northroadian@gmail.com>
2021-06-14 05:10:50 -0500

    adding contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,7 +3,15 @@ import React from 'react'
 const Contact = () => {
     return (
         <div>
-            <h1>HEY YOOHAHAHA</h1>
+          <form>
+              <input type='text' placeholder='First Name'/>
+              <input type='text' placeholder='Last Name'/>
+              <input type='text' placeholder='Email Address'/>
+              <input type='text' placeholder='Phone Number'/>
+              <input type='text-area' placeholder='Enter your message here'/>
+              <button>Submit</button>
+            
+          </form>
         </div>
     )
 }
