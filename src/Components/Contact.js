7f5d01839500efa6164c49d02defa3a6fc4fec08
Ian Hancock <northroadian@gmail.com>
2021-06-15 06:20:08 -0500

    adding to contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react';
 
 const Contact = () => {
+    const [firstName ,setFirstName]= useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [phoneNumber, setPhoneNumber] = useState('')
+    const [message, setMessage] = useState('')
+	
+    
     return (
-        <div>
-          <form>
-              <input type='text' placeholder='First Name'/>
-              <input type='text' placeholder='Last Name'/>
-              <input type='text' placeholder='Email Address'/>
-              <input type='text' placeholder='Phone Number'/>
-              <input type='text-area' placeholder='Enter your message here'/>
-              <button>Submit</button>
-            
-          </form>
-        </div>
-    )
-}
+		<form className="contact-form">
+			<div className="form-group">
+				<lable htmlFor="First Name">First Name</lable>
+				<input type="text" placeholder="First Name" />
+			</div>
+			<div className="form-group">
+				<lable htmlFor="Last Name">Last Name</lable>
+				<input type="text" placeholder="Last Name" />
+			</div>
+			<div className="form-group">
+				<lable htmlFor="Email">Email</lable>
+				<input type="text" placeholder="Email Address" />
+			</div>
+			<div className="form-group">
+				<lable htmlFor="Phone Number">Phone Number</lable>
+				<input type="text" placeholder="Phone Number" />
+			</div>
+			<div className="form-group">
+				<lable htmlFor="Message">Message</lable>
+				<input type="text-area" className="message-box" placeholder="Enter your message here" />
+			</div>
 
-export default Contact
+			<button type="submit" className="btn">
+				Submit
+			</button>
+		</form>
+	);
+};
+
+export default Contact;
