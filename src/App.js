eec41019dff05bc25da0fb342b6067781eb7d1cf
Ian Hancock <northroadian@gmail.com>
2021-06-15 15:29:47 -0500

    progress on validation

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ import Projects from './Components/Projects'
 import Resume from './Components/Resume'
 import Testimonials from './Components/Testimonials'
 import './App.css'
-import {useSelector ,useDispatch} from 'react-redux'
-import {increment, decrement} from './actions'
+// import {useSelector ,useDispatch} from 'react-redux'
+// import {increment, decrement} from './actions'
 
 
 function App() {
-  const counter = useSelector( state => state.counter)
-  const isLogged = useSelector (state => state.isLogged )
-  const dispatch = useDispatch()
+  // const counter = useSelector( state => state.counter)
+  // const isLogged = useSelector (state => state.isLogged )
+  // const dispatch = useDispatch()
   return (
     <div className="App">
 
@@ -33,15 +33,15 @@ function App() {
        <Route path="/testimonials"> <Testimonials/> </Route>
        <Route path="/contact"> <Contact/> </Route>
        <Route path="/"> <Home/> </Route>
-      
+  
        
        </Switch>
      </BrowserRouter>
-     <h1>Counter {counter} </h1>
+     {/* <h1>Counter {counter} </h1>
      <button onClick={() => dispatch(increment())}> + </button>
      <button onClick={()=> dispatch(decrement())}> - </button>
     {isLogged? <h3>valuable info {isLogged}</h3> :'not logged in' }
-    
+     */}
     </div>
     // add button for is logged to toggle logged and unlogged 
   );
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,34 +1,38 @@
-import React from "react";
-import { useForm } from "react-hook-form";
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Contact = () => {
-	const {register, handleSubmit} = useForm()
-    const onSubmit = data => console.log(data) 
-    return (
-		<form onSubmit = {handleSubmit(onSubmit)} className="contact-form">
-			<div className="form-group">
-				<lable htmlFor="First Name">First Name</lable>
-				<input {...register("firstName", { required: true, maxLength: 20 })} />
-			</div>
-			<div className="form-group">
-				<lable htmlFor="Last Name">Last Name</lable>
-			<input {...register('lastName',{required: true, maxLength: 20})}/>
-			</div>
-			<div className="form-group">
-				<lable htmlFor="Email">Email</lable>
-				<input type="text" placeholder="Email Address" />
-			</div>
-			<div className="form-group">
-				<lable htmlFor="Phone Number">Phone Number</lable>
-				<input type="text" placeholder="Phone Number" />
-			</div>
-			<div className="form-group">
-				<lable htmlFor="Message">Message</lable>
-				<input type="text-area" className="message-box" placeholder="Enter your message here" />
-			</div>
-			<input type = 'submit' /> 
-
-			
+	const { register, handleSubmit } = useForm();
+	const onSubmit = (data) => console.log(data);
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<lable>First Name</lable>
+			<input {...register('firstName', { required: true, maxLength: 20 })} />
+			<lable>Last Name</lable>
+			<input {...register('lastName', { pattern: /^[A-Za-z]+$/i })} />
+			<lable>Email</lable>
+			<input
+				type="email"
+				className="input"
+				placeholder="Email"
+				required
+				{...register('email', {
+					required: 'Email is required',
+					pattern: {
+						value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+						message: 'Please enter a valid email'
+					}
+				})}
+			/>
+			<input
+				type="text-area"
+				placeholder="Enter your message here"
+				{...register('text-area', {
+					required: true,
+					maxLength: 750
+				})}
+			/>
+			<input type="submit" />
 		</form>
 	);
 };
